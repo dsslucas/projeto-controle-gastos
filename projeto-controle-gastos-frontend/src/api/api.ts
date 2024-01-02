83d5c08e26b4b2007e1c6c1377864c71b53aa7d1
Lucas Silva <lucas_silva_souza@hotmail.com>
2024-01-02 09:00:55 -0300

    Mudança de IP para conexão

--- projeto-controle-gastos-frontend/src/api/api.ts
+++ projeto-controle-gastos-frontend/src/api/api.ts
@@ -6,8 +6,8 @@ import axios from 'axios';
 // });
 
 const api = axios.create({
-  //baseURL: 'http://192.168.0.113:3003',
-  baseURL: "http://192.168.101.10:3003"
+  baseURL: 'http://192.168.0.113:3003',
+  //baseURL: "http://192.168.101.10:3003"
 });
 
 export default api;
