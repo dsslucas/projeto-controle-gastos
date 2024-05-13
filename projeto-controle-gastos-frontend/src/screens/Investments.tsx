8c05917160f3291c93c18335cdf320454cec767d
unknown <lucasdasilvasou@gmail.com>
2024-05-13 20:47:11 -0300

    feat: Remoção de modal para adição de investimento

--- projeto-controle-gastos-frontend/src/screens/Investments.tsx
+++ projeto-controle-gastos-frontend/src/screens/Investments.tsx
@@ -5,42 +5,15 @@ import Title from "../components/text/Title";
 import api from "../api/api";
 import Alert from "../components/alert/Alert";
 import Button from "../components/button/Button";
-import ModalRegisterInvestment from "../components/modal/ModalRegisterInvestment";
 import ModalRescueInvestment from "../components/modal/ModalRescueInvestment";
-import globalFunctions from "../global/functions";
 
 const Investments = (props: any) => {
     const [dataApiInvestment, setDataApiInvestment] = useState<any>();
-    const [showModalRegisterInvestment, setShowModalRegisterInvestment] = useState(false);
     const [showModalRescueInvestment, setShowModalRescueInvestment] = useState(false);
-    const { optionsInvestments } = globalFunctions();
 
     useEffect(() => {
         getData("Investimentos", "", "");
     }, []);
-
-    // POST INVESTMENT
-    const createInvestment = async (response: any) => {
-        console.log(response)
-
-        await api.post("/investment", response)
-            .then((response: any) => {
-                console.log(response.data)
-                Alert({
-                    text: response.data,
-                    icon: "success",
-                    //callback: setShowModalRegisterInvestment(false)
-                    callback: null
-                });
-            })
-            .catch((error: any) => {
-                console.error(error)
-                Alert({
-                    text: error.response.data,
-                    icon: "error"
-                });
-            })
-    }
 
     const getData = async (category: string, paymentMethod: string, date: string) => {
         console.log("O QUE ESTOU RECEBENDO: ", date)
@@ -56,14 +29,6 @@ const Investments = (props: any) => {
 
     return (
         <>
-            {showModalRegisterInvestment && (
-                <ModalRegisterInvestment
-                    options={optionsInvestments}
-                    sendData={(data: any) => createInvestment(data)}
-                    returnClick={() => setShowModalRegisterInvestment(false)}
-                    returnAlert={(alert: any) => Alert(alert)}
-                />
-            )}
             {showModalRescueInvestment && (
                 <ModalRescueInvestment
                     returnClick={() => setShowModalRescueInvestment(false)}
@@ -74,9 +39,6 @@ const Investments = (props: any) => {
                     <Title title="Investimentos" />
                 </div>
                 <div className="flex gap-2">
-                    <Button type="button" content="Cadastrar" color="bg-green-500 text-white" returnClick={() => {
-                        setShowModalRegisterInvestment(true);
-                    }} />
                     <Button type="button" content="Resgatar" color="bg-red-500 text-white" returnClick={() => {
                         setShowModalRescueInvestment(true)
                     }} />
